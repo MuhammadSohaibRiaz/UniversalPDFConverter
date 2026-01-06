5b7b6fd8576ee6351c5f4dc0b2770a0fb7ed7bd3
MuhammadSohaibRiaz <sohaibriaz201@gmail.com>
2026-01-07 01:31:00 +0500

    deploying on cpanel

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
